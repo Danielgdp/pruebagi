33114bc790142b402ca6f4ff3738ecfa9dffd859
Anel Noreña <anelnorena@macbook-air-de-anel.local>
2016-08-18 16:17:48 -0500

    Initial Commit

---
+++ pruebagit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebagit
+//
+//  Created by Anel Noreña on 18/08/16.
+//  Copyright © 2016 Anel Noreña. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
